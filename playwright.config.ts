0969d64376fc2894cfecf1fdeadc53ded125884d
Dmitry Afonasenko <dmitrytbp@gmail.com>
2026-08-30 18:18:17 +0500

    ci: increase browser test parallelism

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 2 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   reporter: process.env.CI ? 'github' : 'list',
   snapshotPathTemplate: '{testDir}/{testFilePath}-snapshots/{arg}-{projectName}{ext}',
   use: {
